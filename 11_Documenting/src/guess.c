50231745c90bad2ee2f1ad8068cce813231b74e5
VasilyMozgovykh <vasily.mozgovykh@yandex.ru>
2025-12-27 15:05:01 +0000

    Added roman numbers support

--- 11_Documenting/src/guess.c
+++ 11_Documenting/src/guess.c
@@ -8,20 +8,76 @@
 
 #define _(STRING) gettext(STRING)
 
-int main(void) {
+/* Roman numbers support */
+typedef struct RomanNumeralPair {
+    const char *roman;
+    int numeral;
+} RomanNumeralPair;
+
+RomanNumeralPair RomanNumeralMap[] = {
+    {"C", 100}, {"XC", 90}, {"L", 50},
+    {"XL", 40},{"X", 10}, {"IX", 9},
+    {"V", 5}, {"IV", 4}, {"I", 1}
+};
+
+char *numeral_to_roman(int numeral) {
+    static char roman[10];
+    char *roman_ptr = roman;
+    for (int i = 0; i < 9; i++) {
+        int div = numeral / RomanNumeralMap[i].numeral;
+        for (int k = 0; k < div; k++) {
+            for (int c = 0; c < strlen(RomanNumeralMap[i].roman); c++, roman_ptr++) {
+                *roman_ptr = RomanNumeralMap[i].roman[c];
+            }
+        }
+        numeral %= RomanNumeralMap[i].numeral;
+        if (numeral == 0) {
+            break;
+        }
+    }
+    *roman_ptr = '\0';
+    return roman;
+}
+
+int roman_to_numeral(const char *roman) {
+    int numeral = 0, max_iterations = 10;
+    for (int iter_num = 0; iter_num < max_iterations && *roman != '\0'; iter_num++) {
+        for (int i = 0; i < 9; i++) {
+            if (strstr(roman, RomanNumeralMap[i].roman) == roman) {
+                numeral += RomanNumeralMap[i].numeral;
+                roman += strlen(RomanNumeralMap[i].roman);
+                break;
+            }
+        }
+    }
+    return numeral;
+}
+/* ===================== */
+
+int main(int argc, char **argv) {
     setlocale(LC_ALL, "");
     bindtextdomain(PACKAGE, LOCALE_PATH);
     textdomain(PACKAGE);
 
+    char roman_mode = (argc > 1 && strcmp(argv[1], "-r") == 0);
+
     int low = 1, high = 100, guess;
     char input[10];
 
-    printf(_("Guess a number from 1 to 100.\n"));
+    if (roman_mode) {
+        printf(_("Guess a number from I to C.\n"));
+    } else {
+        printf(_("Guess a number from 1 to 100.\n"));
+    }
     getchar();
 
     while (low < high) {
         guess = (low + high) / 2;
-        printf(_("Is your number greater than %d? (Yes/No): "), guess);
+        if (roman_mode) {
+            printf(_("Is your number greater than %s? (Yes/No): "), numeral_to_roman(guess));
+        } else {
+            printf(_("Is your number greater than %d? (Yes/No): "), guess);
+        }
 
         if (fgets(input, sizeof(input), stdin) == NULL) {
             fprintf(stderr, _("Input/Output error. Terminating...\n"));
@@ -37,6 +93,10 @@ int main(void) {
         }
     }
 
-    printf(_("Your guessed number is %d!\n"), (low + high) / 2);
+    if (roman_mode) {
+        printf(_("Your guessed number is %s!\n"), numeral_to_roman((low + high) / 2));
+    } else {
+        printf(_("Your guessed number is %d!\n"), (low + high) / 2);
+    }
     return 0;
 }
