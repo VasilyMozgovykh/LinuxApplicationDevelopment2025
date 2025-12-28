66eeab3d8d9993691f64795471e08916ce49376e
VasilyMozgovykh <vasily.mozgovykh@yandex.ru>
2025-12-28 11:49:44 +0000

    Updated translation

--- 12_InstallPackaging/src/guess.c
+++ 12_InstallPackaging/src/guess.c
@@ -93,9 +93,9 @@ int main(int argc, char **argv) {
             return 1;
         }
 
-        if (strncmp(input, "Yes", 3) == 0) {
+        if (strncmp(input, _("Yes"), 3) == strlen(_("Yes"))) {
             low = guess + 1;
-        } else if (strncmp(input, "No", 2) == 0) {
+        } else if (strncmp(input, _("No"), 2) == strlen(_("No"))) {
             high = guess;
         } else {
             printf(_("Please answer 'Yes' or 'No'\n"));
